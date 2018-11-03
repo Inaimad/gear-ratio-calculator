919de4834eea6241142c1568256ba1e8da541a68
Damian Igielski <damian.igielski@outlook.com>
2018-11-03 08:58:01 +0100

    Initial commit

---
+++ src/main/java/com/gearcalc/gearratiocalculator/GearRatioCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.gearcalc.gearratiocalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearRatioCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearRatioCalculatorApplication.class, args);
+    }
+}
